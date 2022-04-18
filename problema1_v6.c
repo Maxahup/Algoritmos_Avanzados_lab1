8352ec31b246600d463000e60cdeabd5f8ec1916
Maxahup <64162650+Maxahup@users.noreply.github.com>
2022-04-17 21:49:03 -0400

    cambios finales, se agregan comentarios

--- problema1_v6.c
+++ problema1_v6.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 
-int factorial( int n){
+int factorial( int n){									//funcion para obtener el factorial de un numero
 	if (n==0){
 		return 1;
 	}
@@ -14,7 +14,7 @@ int factorial( int n){
 	}
 }
 
-int combination(int n, int m){
+int combination(int n, int m){							//funcion para obtener la combinatoria de dos valores
 	if (n<=0 || m<=0){
 		return 0;
 	}
@@ -24,7 +24,7 @@ int combination(int n, int m){
 	}
 }
 
-int **getData(char* nombreArchivo){
+int **getData(char* nombreArchivo){						//funcion para la lectura del archivo de entrada
 	char fileName[20];
 	int largo=strlen(nombreArchivo);
 	strcpy(fileName,nombreArchivo);
@@ -41,12 +41,12 @@ int **getData(char* nombreArchivo){
 		exit(-1);
 	}
 	else{
-		//reserve of memory
+		//reserva de memoria
 		int** data = (int**)malloc(sizeof(int*)*sizeData);
 		for (int i = 0; i < sizeData; ++i){
 			data[i]=(int *)malloc(sizeof(int)*2);
 		}
-		//reading data
+		//lectura de datos
 		for(int i = 0; i<sizeData; i++){
 	        for(int j = 0 ; j<2; j++){
 	            fscanf(fp, "%i", &data[i][j]);
@@ -57,7 +57,7 @@ int **getData(char* nombreArchivo){
 	}
 }
 
-bool breaker(int* combinations,int dataSize){
+bool breaker(int* combinations,int dataSize){					//funcion complementaria para detener ciclo de busqueda
 	for(int i=0; i < dataSize; i++){
 		if(combinations[i] == 0){
 			return false;
@@ -68,17 +68,17 @@ bool breaker(int* combinations,int dataSize){
 
 void getMaxiumValue(int** data, int dataSize, int maxPonderation){
 
-	int maxiumValues[2] ={0,0};				//save the max value and ponderation found
-	int* combinationValues = (int *)malloc(sizeof(int)*dataSize);	//create an array to save combinations, always of data´s size
-	int* combinationPonderation = (int *)malloc(sizeof(int)*dataSize);	//analogous to the above
-	for(int i = 0 ; i < dataSize ; i++){		//initialize arrays with value 0 
+	int maxiumValues[2] ={0,0};				//arreglo para guardar el valor maximo y la ponderacion
+	int* combinationValues = (int *)malloc(sizeof(int)*dataSize);	//arreglo para guardar el valor de los datos en binario
+	int* combinationPonderation = (int *)malloc(sizeof(int)*dataSize);	//arreglo para guardar la ponderacion de la combinacion
+	for(int i = 0 ; i < dataSize ; i++){		//asignacion de valor 0 a los arreglos
 		combinationValues[i] = 0;
 		combinationPonderation[i] = 0;
 	}
-	while(!breaker(combinationValues, dataSize)){		//process to set values to arrays with binary values and then evaluate them with values of the original data
+	while(!breaker(combinationValues, dataSize)){		//proceso para fijar valores a los arreglos en binario y evaluarlos con los datos leidos
 		bool keep = true;
 		int CombinationSize = dataSize-1;
-		int actualValues[2] ={0,0};						//array that save the value of the combination observed
+		int actualValues[2] ={0,0};						//arreglo con el valor y la ponderacion de la combinacion actual
 
 		while(keep){
 			if(combinationValues[CombinationSize] == 0){
@@ -90,30 +90,30 @@ void getMaxiumValue(int** data, int dataSize, int maxPonderation){
 				CombinationSize--; 
 			}
 		}
-		for (int i = 0; i < dataSize; ++i){			//collect the values comparing the position of the combination created and the data
+		for (int i = 0; i < dataSize; ++i){			//recorre la combinacion binaria creada y suma el valor de las posiciones con un valor 1
 			if (combinationValues[i]==1){
 				actualValues[0]=actualValues[0]+data[i][0];
 				actualValues[1]=actualValues[1]+data[i][1];
 			}
 		}
-		if(actualValues[0] > maxiumValues[0] && actualValues[1] <= maxPonderation){		//check if the actual combination it's a new max value and replace if this is true
+		if(actualValues[0] > maxiumValues[0] && actualValues[1] <= maxPonderation){		//comparacion para verificar si el valor encontrado en la combinacion es el nuevo maximo
 			maxiumValues[0]=actualValues[0];
 			maxiumValues[1]=actualValues[1];
 		}
 	}
 
 	free(combinationValues);
-	printf("max value found: %i with ponderation: %i\n", maxiumValues[0], maxiumValues[1] );
+	printf("maximo valor encontrado: %i con ponderacion: %i\n", maxiumValues[0], maxiumValues[1] );
 }
 
-int getAmountData(char *fileName){		
+int getAmountData(char *fileName){					//funcion para obtener desde el nombre del archivo la cantidad de datos
 	char* trash=strtok(fileName,"_");
 	char* charAmountData=strtok(NULL,"_");
 	int amountData=atoi(charAmountData);
 	return amountData;
 }
 
-int getMaxPonderation(char *nombreArchivo){
+int getMaxPonderation(char *nombreArchivo){			//funcion para obtener desde el nombre del archivo la ponderacion maxima
 	char fileName[20];
 	int largo=strlen(nombreArchivo);
 	strcpy(fileName,nombreArchivo);
@@ -129,22 +129,23 @@ int getMaxPonderation(char *nombreArchivo){
 
 int main(int argc, char const *argv[]){
 	
-	time_t start, end;
+	//PROCESO DE DECLARACION DE VARIABLES
+	time_t start, end;				//valores para el conteo del tiempo de ejecucion del algoritmo
 	char name[20];
 	char copyName[20];
 	printf("Ingrese el nombre del archivo con extension '.txt': \n");
 	scanf("%s", name);
 	strcpy(copyName,name);
 
-	//DATA COLLECTION PROCESS
+	//PROCESO DE OBTENCION DE DATOS
 	int** data=getData(name);
 	int amountData = getAmountData(name);
 	int maxPonderation=getMaxPonderation(copyName);
 	
-	//MAXIUM VALUE SEARCH PROCESS
-	start = time(NULL);
+	//PROCESO DE BUSQUEDA DE VALOR MAXIMO 
+	start = time(NULL);						//fijacion de tiempo de inicio 
 	getMaxiumValue(data,amountData,maxPonderation);
-	end = time(NULL);
+	end = time(NULL);						//detencion de la medicion del tiempo
 	printf("Tiempo de ejecucion algoritmo: %f[s].\n",  difftime(end, start));
 
 	return 0;
